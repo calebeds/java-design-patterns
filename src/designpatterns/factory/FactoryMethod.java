a826c842ed08990ad11808b9fcc6aa1aefaa6c84
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2026-03-02 19:24:16 -0300

    add factory method example

---
+++ src/designpatterns/factory/FactoryMethod.java
@@ -0,0 +1,24 @@
+package designpatterns.factory;
+
+class Point {
+    private double x, y;
+
+    private Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public static Point newCartesianPoint(double x, double y) {
+        return new Point(x, y);
+    }
+
+    public static Point newPolarPoint(double rho, double theta) {
+        return new Point(rho * Math.cos(theta), rho * Math.sin(theta));
+    }
+}
+
+public class FactoryMethod {
+    public static void main(String[] args) {
+        Point point = Point.newPolarPoint(2, 3);
+    }
+}
