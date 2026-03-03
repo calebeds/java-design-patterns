72036c5cb28ef19da0c1f98f79dc70856b6693da
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2026-03-03 11:42:53 -0300

    add serialization with singleton example

--- src/designpatterns/singleton/BasicSingletonEx.java
+++ src/designpatterns/singleton/BasicSingletonEx.java
@@ -1,6 +1,8 @@
 package designpatterns.singleton;
 
-class BasicSingleton {
+import java.io.*;
+
+class BasicSingleton implements Serializable {
     private static final BasicSingleton INSTANCE = new BasicSingleton();
 
     private int value = 0;
@@ -20,12 +22,42 @@ class BasicSingleton {
     public static BasicSingleton getInstance() {
         return INSTANCE;
     }
+
+    protected Object readResolve() {
+        return INSTANCE;
+    }
 }
 
 public class BasicSingletonEx {
-    public static void main(String[] args) {
+
+    static void saveToFile(BasicSingleton singleton, String filename) throws Exception {
+        try(FileOutputStream fileOutputStream = new FileOutputStream(filename);
+            ObjectOutputStream out = new ObjectOutputStream(fileOutputStream)) {
+            out.writeObject(singleton);
+        }
+    }
+
+    static BasicSingleton readFromFile(String filename) throws Exception {
+        try(FileInputStream fileInputStream = new FileInputStream(filename);
+            ObjectInputStream in = new ObjectInputStream(fileInputStream)) {
+            return (BasicSingleton) in.readObject();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
         BasicSingleton singleton = BasicSingleton.getInstance();
         singleton.setValue(123);
+
+        String filename = "singleton.bin";
+        saveToFile(singleton, filename);
+
+        singleton.setValue(222);
+
+        BasicSingleton singleton2 = readFromFile(filename);
+
+        System.out.println(singleton == singleton2);
+
         System.out.println(singleton.getValue());
+        System.out.println(singleton2.getValue());
     }
 }
