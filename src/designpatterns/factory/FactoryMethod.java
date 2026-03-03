6e7bf8601dcfa6cf52e90404f24a87314c99ff75
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2026-03-02 21:18:52 -0300

    add static factory example

--- src/designpatterns/factory/FactoryMethod.java
+++ src/designpatterns/factory/FactoryMethod.java
@@ -8,17 +8,21 @@ class Point {
         this.y = y;
     }
 
-    public static Point newCartesianPoint(double x, double y) {
-        return new Point(x, y);
-    }
+    static class Factory {
+        public static Point newCartesianPoint(double x, double y) {
+            return new Point(x, y);
+        }
 
-    public static Point newPolarPoint(double rho, double theta) {
-        return new Point(rho * Math.cos(theta), rho * Math.sin(theta));
+        public static Point newPolarPoint(double rho, double theta) {
+            return new Point(rho * Math.cos(theta), rho * Math.sin(theta));
+        }
     }
 }
 
+
+
 public class FactoryMethod {
     public static void main(String[] args) {
-        Point point = Point.newPolarPoint(2, 3);
+        Point point = Point.Factory.newPolarPoint(2, 3);
     }
 }
