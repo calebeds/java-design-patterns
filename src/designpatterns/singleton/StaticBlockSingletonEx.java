51a09835c2745ebcbccff385fd6f97831e85fb3d
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2026-03-03 21:09:47 -0300

    add laziness and thread safety example

---
+++ src/designpatterns/singleton/StaticBlockSingletonEx.java
@@ -0,0 +1,28 @@
+package designpatterns.singleton;
+
+import java.io.File;
+import java.io.IOException;
+
+class StaticBlockSingleton {
+    private static StaticBlockSingleton instance;
+
+    private StaticBlockSingleton() throws IOException {
+        System.out.println("Singleton is initializing");
+        File.createTempFile(".", ".");
+    }
+
+    static {
+        try {
+            instance = new StaticBlockSingleton();
+        } catch (Exception e) {
+            System.err.println("failed to create singleton");
+        }
+    }
+
+    public static StaticBlockSingleton getInstance() {
+        return instance;
+    }
+}
+
+public class StaticBlockSingletonEx {
+}
