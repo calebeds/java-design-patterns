cac4b37c1e0e829e0807153554d7846ee4a28d4f
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2026-03-09 11:26:05 -0300

    add adapter decorator example

---
+++ src/designpatterns/decorator/AdapterDecorator.java
@@ -0,0 +1,260 @@
+package designpatterns.decorator;
+
+
+import java.util.stream.IntStream;
+
+class MyStringBuilder {
+    private StringBuilder stringBuilder;
+
+    public MyStringBuilder() {
+        this.stringBuilder = new StringBuilder();
+    }
+
+    public MyStringBuilder(String string) {
+        this.stringBuilder = new StringBuilder(string);
+    }
+
+    public MyStringBuilder concat(String str) {
+        return new MyStringBuilder(stringBuilder.toString().concat(str));
+    }
+
+    public MyStringBuilder appendLine(String str) {
+        stringBuilder.append(str).append(System.lineSeparator());
+        return this;
+    }
+
+    // decorator
+    public int compareTo(StringBuilder another) {
+        return stringBuilder.compareTo(another);
+    }
+
+    public MyStringBuilder append(StringBuffer sb) {
+        stringBuilder.append(sb);
+        return this;
+    }
+
+    public int lastIndexOf(String str, int fromIndex) {
+        return stringBuilder.lastIndexOf(str, fromIndex);
+    }
+
+    public IntStream chars() {
+        return stringBuilder.chars();
+    }
+
+    public StringBuilder insert(int offset, int i) {
+        return stringBuilder.insert(offset, i);
+    }
+
+    public StringBuilder append(float f) {
+        return stringBuilder.append(f);
+    }
+
+    public StringBuilder insert(int offset, Object obj) {
+        return stringBuilder.insert(offset, obj);
+    }
+
+    public int codePointBefore(int index) {
+        return stringBuilder.codePointBefore(index);
+    }
+
+    public StringBuilder replace(int start, int end, String str) {
+        return stringBuilder.replace(start, end, str);
+    }
+
+    public int indexOf(String str, int fromIndex) {
+        return stringBuilder.indexOf(str, fromIndex);
+    }
+
+    public StringBuilder append(boolean b) {
+        return stringBuilder.append(b);
+    }
+
+    public void trimToSize() {
+        stringBuilder.trimToSize();
+    }
+
+    public MyStringBuilder append(String str) {
+        stringBuilder.append(str);
+        return this;
+    }
+
+    public StringBuilder insert(int offset, boolean b) {
+        return stringBuilder.insert(offset, b);
+    }
+
+    public char charAt(int index) {
+        return stringBuilder.charAt(index);
+    }
+
+    public StringBuilder insert(int dstOffset, CharSequence s) {
+        return stringBuilder.insert(dstOffset, s);
+    }
+
+    public StringBuilder delete(int start, int end) {
+        return stringBuilder.delete(start, end);
+    }
+
+    public StringBuilder insert(int offset, double d) {
+        return stringBuilder.insert(offset, d);
+    }
+
+    public StringBuilder repeat(CharSequence cs, int count) {
+        return stringBuilder.repeat(cs, count);
+    }
+
+    public StringBuilder append(double d) {
+        return stringBuilder.append(d);
+    }
+
+    public StringBuilder insert(int offset, long l) {
+        return stringBuilder.insert(offset, l);
+    }
+
+    public StringBuilder insert(int offset, String str) {
+        return stringBuilder.insert(offset, str);
+    }
+
+    public int capacity() {
+        return stringBuilder.capacity();
+    }
+
+    public StringBuilder append(char[] str) {
+        return stringBuilder.append(str);
+    }
+
+    public StringBuilder reverse() {
+        return stringBuilder.reverse();
+    }
+
+    public CharSequence subSequence(int start, int end) {
+        return stringBuilder.subSequence(start, end);
+    }
+
+    public int offsetByCodePoints(int index, int codePointOffset) {
+        return stringBuilder.offsetByCodePoints(index, codePointOffset);
+    }
+
+    public StringBuilder insert(int offset, char c) {
+        return stringBuilder.insert(offset, c);
+    }
+
+    public int codePointAt(int index) {
+        return stringBuilder.codePointAt(index);
+    }
+
+    public String substring(int start) {
+        return stringBuilder.substring(start);
+    }
+
+    public StringBuilder append(int i) {
+        return stringBuilder.append(i);
+    }
+
+    public boolean isEmpty() {
+        return stringBuilder.isEmpty();
+    }
+
+    public StringBuilder append(CharSequence s) {
+        return stringBuilder.append(s);
+    }
+
+    public int length() {
+        return stringBuilder.length();
+    }
+
+    public int lastIndexOf(String str) {
+        return stringBuilder.lastIndexOf(str);
+    }
+
+    public StringBuilder append(long lng) {
+        return stringBuilder.append(lng);
+    }
+
+    public void setLength(int newLength) {
+        stringBuilder.setLength(newLength);
+    }
+
+    public StringBuilder append(Object obj) {
+        return stringBuilder.append(obj);
+    }
+
+    public void setCharAt(int index, char ch) {
+        stringBuilder.setCharAt(index, ch);
+    }
+
+    public StringBuilder insert(int dstOffset, CharSequence s, int start, int end) {
+        return stringBuilder.insert(dstOffset, s, start, end);
+    }
+
+    public StringBuilder insert(int index, char[] str, int offset, int len) {
+        return stringBuilder.insert(index, str, offset, len);
+    }
+
+    public int indexOf(String str) {
+        return stringBuilder.indexOf(str);
+    }
+
+    public StringBuilder append(char c) {
+        return stringBuilder.append(c);
+    }
+
+    public StringBuilder append(char[] str, int offset, int len) {
+        return stringBuilder.append(str, offset, len);
+    }
+
+    public void getChars(int srcBegin, int srcEnd, char[] dst, int dstBegin) {
+        stringBuilder.getChars(srcBegin, srcEnd, dst, dstBegin);
+    }
+
+    public StringBuilder insert(int offset, float f) {
+        return stringBuilder.insert(offset, f);
+    }
+
+    public StringBuilder deleteCharAt(int index) {
+        return stringBuilder.deleteCharAt(index);
+    }
+
+    public StringBuilder repeat(int codePoint, int count) {
+        return stringBuilder.repeat(codePoint, count);
+    }
+
+    public StringBuilder append(CharSequence s, int start, int end) {
+        return stringBuilder.append(s, start, end);
+    }
+
+    public void ensureCapacity(int minimumCapacity) {
+        stringBuilder.ensureCapacity(minimumCapacity);
+    }
+
+    public String substring(int start, int end) {
+        return stringBuilder.substring(start, end);
+    }
+
+    public int codePointCount(int beginIndex, int endIndex) {
+        return stringBuilder.codePointCount(beginIndex, endIndex);
+    }
+
+    public StringBuilder insert(int offset, char[] str) {
+        return stringBuilder.insert(offset, str);
+    }
+
+    public StringBuilder appendCodePoint(int codePoint) {
+        return stringBuilder.appendCodePoint(codePoint);
+    }
+
+    public IntStream codePoints() {
+        return stringBuilder.codePoints();
+    }
+
+    public String toString() {
+        return stringBuilder.toString();
+    }
+}
+
+public class AdapterDecorator {
+    public static void main(String[] args) {
+        MyStringBuilder myStringBuilder = new MyStringBuilder();
+        myStringBuilder.append("hello").appendLine(" world");
+        System.out.println(myStringBuilder.concat("and this too"));
+    }
+}
