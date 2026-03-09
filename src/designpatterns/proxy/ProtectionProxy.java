1ab006e899a87af5cc8f8ed69a66c8a2f69e13ce
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2026-03-09 15:06:16 -0300

    add protection proxy example

---
+++ src/designpatterns/proxy/ProtectionProxy.java
@@ -0,0 +1,57 @@
+package designpatterns.proxy;
+
+interface Drivable {
+    void drive();
+}
+
+class Car implements Drivable {
+    protected Driver driver;
+
+    public Car(Driver driver) {
+        this.driver = driver;
+    }
+
+    @Override
+    public void drive() {
+        System.out.println("Car being driven");
+    }
+}
+
+class Driver {
+    private int age;
+
+    public Driver(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
+class CarProxy extends Car {
+
+    public CarProxy(Driver driver) {
+        super(driver);
+    }
+
+    @Override
+    public void drive() {
+        if(driver.getAge() >= 16) {
+            super.drive();
+        } else {
+            System.out.println("Driver too young!");
+        }
+    }
+}
+
+public class ProtectionProxy {
+    public static void main(String[] args) {
+        Car car = new CarProxy(new Driver(12));
+        car.drive();
+    }
+}
